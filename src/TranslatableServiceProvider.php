698f80b22d1536598a69873ba4142af502914031
askaoru <askaoru@users.noreply.github.com>
2017-06-29 09:24:11 +0800

    Create TranslatableServiceProvider.php

---
+++ src/TranslatableServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Askaoru\Translatable;
+
+use Illuminate\Support\ServiceProvider;
+
+class TranslatableServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/config/translatable.php' => config_path('/translatable.php'),
+            ], 'config');
+
+            $this->publishes([
+                __DIR__.'/migrations/' => database_path('/migrations'),
+            ], 'migrations');
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->make('Askaoru\Translatable\TranslatableController');
+    }
+}
