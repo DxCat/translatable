64286fcad6ef415df2549a7beb241443b45cfa7c
askaoru <askaoru@users.noreply.github.com>
2017-07-01 23:30:37 +0800

    Create 2017_07_01_031473_create_model_translations_table.php

---
+++ src/migrations/2017_07_01_031473_create_model_translations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModelTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('model_translations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type');
+            $table->text('translation');
+            $table->string('locale');
+            $table->string('model');
+            $table->integer('model_id');
+            $table->timestamps();
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('model_translations');
+    }
+}
